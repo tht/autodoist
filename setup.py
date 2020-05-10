0274ddecc769de495726257755c03d8c3e1c5872
Alexander Haselhoff <alexander.haselhoff@outlook.com>
2020-05-10 15:09:31 +0200

    setup.py update

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='NextAction',
-    version='0.5',
-    py_modules=['nextaction'],
-    url='https://github.com/nikdoof/NextAction',
+    name='autodoist',
+    version='1.0',
+    py_modules=['autodoist'],
+    url='https://github.com/Hoffelhas/automation-todoist',
     license='MIT',
-    author='Andrew Williams',
-    author_email='andy@tensixtyone.com',
-    description='A more GTD-like workflow for Todoist. Uses the REST API to add and remove a @next_action label from tasks.',
-    entry_points={
-        "console_scripts": [
-            "nextaction=nextaction:main",
-        ],
-    },
+    author='Alexander Haselhoff',
+    author_email='alexander.haselhoff@outlook.com',
+    description='Added functionality for Todoist: 1) next-action labels and 2) re-use recurring sub-tasks',
     install_requires=[
         'todoist-python',
     ]
