d129885bbcaa10d9be19af2652b285948c379593
Hoffelhas <xela@live.nl>
2021-01-23 21:53:17 +0100

    setup.py update

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name='autodoist',
-    version='1.0',
+    version='1.5',
     py_modules=['autodoist'],
     url='https://github.com/Hoffelhas/automation-todoist',
     license='MIT',
     author='Alexander Haselhoff',
     author_email='alexander.haselhoff@outlook.com',
-    description='Added functionality for Todoist: 1) next-action labels and 2) re-use recurring sub-tasks',
+    description='Added functionality for Todoist: 1) next-action labels, 2) sub-task regeneration, 3) postpone end of day, and 4) (un)header items simultaneously)',
     install_requires=[
         'todoist-python',
+        'requests',
     ]
 )
